21ac0218e64494f84278bffc5c5e98e81119f2f0
Chris Holden <chris.holden@rainbird.ai>
2017-02-13 16:50:47 +0000

    Removed use of WordNet from lexicalInfo.

--- test/lib/lexicalInfo.test.js
+++ test/lib/lexicalInfo.test.js
@@ -4,28 +4,6 @@ var lex = require('../../lib/lexicalInfo');
 
 describe('lexicalInfo', function() {
 
-    describe('findRoots()', function () {
-
-        it('should return the expected data for the word:likes', function(done) {
-
-            lex.findRoots('likes', function(err, result) {
-                expect(result.verbRoot).to.equal('like');
-
-                done();
-            });
-        });
-
-        it('should return the expected data for the word:like', function(done) {
-
-            lex.findRoots('like', function(err, result) {
-                expect(result.verbRoot).to.equal('like');
-
-                done();
-            });
-        });
-
-    });
-
     describe('lookupRoot()', function () {
 
         it('should return the expected data for the word:like', function(done) {
